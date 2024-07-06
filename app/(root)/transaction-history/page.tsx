bc369d91be2824905686cfd27640402d74a57aa9
pritam16 <pritammajumder416@gmail.com>
2024-07-07 03:58:56 +0530

    implement the complete layout and bank cards

---
+++ app/(root)/transaction-history/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const TransactionHistory = () => {
+  return <div>TransactionHistory</div>;
+};
+
+export default TransactionHistory;
--- components/BankCard.tsx
+++ components/BankCard.tsx
@@ -10,33 +10,37 @@ const BankCard = ({
 }: CreditCardProps) => {
   return (
     <div className="flex flex-col">
-      <Link href={"/"} className="bank-card">
-        <div className="bank-card_content">
+      <Link
+        href={"/"}
+        className="relative flex h-48 w-full max-w-[320px] justify-between rounded-[20px] border border-white bg-bank-gradient shadow-creditCard backdrop-blur-[6px]"
+      >
+        <div className="relative z-10 flex size-full  flex-col justify-between rounded-l-[20px]  bg-bank-gradient px-5 pb-4 pt-5">
           <div>
-            <span className="text-16 font-semibold text-white">
+            <span className="text-base whitespace-nowrap font-semibold text-white">
               {account.name || userName}
             </span>
             <p className="font-ibm-plex-serif font-black text-white">
               {formatAmount(account.currentBalance)}
             </p>
           </div>
-          <article className="flex flex-col gap-2">
+          <div className="flex flex-col gap-2">
             <div className="flex justify-between">
-              <span className="text-12 font-semibold text-white">
+              <span className="text-xs font-semibold text-white">
                 {userName}
               </span>
 
-              <span className="text-12 font-semibold text-white">◍◍/◍◍</span>
+              <span className="text-xs font-semibold text-white">●●/●●</span>
             </div>
-            <span className="text-14 font-semibold tracking-[1.1px] text-white">
-              ◍◍◍◍ ◍◍◍◍ ◍◍◍◍ {1234} {account.mask}
+            <span className=" text-sm whitespace-nowrap font-semibold tracking-[1.1px] text-white">
+              ●●●● ●●●● ●●●● <span className="text-base">{1234}</span>
             </span>
-          </article>
+          </div>
         </div>
-        <div className="bank-card_icon">
+        <div className="flex size-full flex-col items-end justify-between rounded-r-[20px] bg-bank-gradient bg-cover bg-center bg-no-repeat py-5  pr-5 ">
           <Image
             src={"/icons/Paypass.svg"}
             alt={"pay"}
+            className="size-8"
             width={20}
             height={24}
           />
@@ -44,15 +48,16 @@ const BankCard = ({
             src={"/icons/mastercard.svg"}
             alt={"mastercard"}
             width={45}
+            className="ml-1"
             height={32}
           />
         </div>
         <Image
-          src={"/icons/lines.png"}
+          src={"/icons/lines.svg"}
           alt={"lines"}
           width={316}
           height={190}
-          className=" ml-5"
+          className="absolute top-0 left-0"
         />
       </Link>
     </div>
