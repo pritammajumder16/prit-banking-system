93108fa17d2a486a87421ed54ebd6353d879590a
pritam16 <pritammajumder416@gmail.com>
2024-08-03 13:38:32 +0530

    sentry re-wizard run

--- app/sentry-example-page/page.tsx
+++ app/sentry-example-page/page.tsx
@@ -49,18 +49,15 @@ export default function Page() {
             margin: "18px",
           }}
           onClick={async () => {
-            await Sentry.startSpan(
-              {
-                name: "Example Frontend Span",
-                op: "test",
-              },
-              async () => {
-                const res = await fetch("/api/sentry-example-api");
-                if (!res.ok) {
-                  throw new Error("Sentry Example Frontend Error");
-                }
+            await Sentry.startSpan({
+              name: 'Example Frontend Span',
+              op: 'test'
+            }, async () => {
+              const res = await fetch("/api/sentry-example-api");
+              if (!res.ok) {
+                throw new Error("Sentry Example Frontend Error");
               }
-            );
+            });
           }}
         >
           Throw error!
@@ -68,10 +65,7 @@ export default function Page() {
 
         <p>
           Next, look for the error on the{" "}
-          <a href="https://pritam-majumder.sentry.io/issues/?project=4507561761767504">
-            Issues Page
-          </a>
-          .
+          <a href="https://pritam-majumder.sentry.io/issues/?project=4507561761767504">Issues Page</a>.
         </p>
         <p style={{ marginTop: "24px" }}>
           For more information, see{" "}
--- instrumentation.ts
+++ instrumentation.ts
@@ -3,19 +3,9 @@ import dbConnect from "./lib/connectDB";
 import * as Sentry from "@sentry/nextjs";
 export async function register() {
   await dbConnect();
-  if (process.env.NEXT_RUNTIME === "nodejs") {
-    Sentry.init({
-      dsn: "https://cedb0f28d201239cfb1eb7651e3f202d@o4507561757376512.ingest.de.sentry.io/4507561761767504",
-      tracesSampleRate: 1,
-      debug: false,
-    });
-  }
-
-  if (process.env.NEXT_RUNTIME === "edge") {
-    Sentry.init({
-      dsn: "https://cedb0f28d201239cfb1eb7651e3f202d@o4507561757376512.ingest.de.sentry.io/4507561761767504",
-      tracesSampleRate: 1,
-      debug: false,
-    });
-  }
+  Sentry.init({
+    dsn: "https://cedb0f28d201239cfb1eb7651e3f202d@o4507561757376512.ingest.de.sentry.io/4507561761767504",
+    tracesSampleRate: 1,
+    debug: false,
+  });
 }
